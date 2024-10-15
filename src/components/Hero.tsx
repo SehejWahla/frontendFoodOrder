7dd85b6fc1ea6e8b087ed1e9ccd76dfa27312391
Sehej Wahla <sehejwahla@gmail.com>
2024-10-16 09:19:40 +1100

    landing page

---
+++ src/components/Hero.tsx
@@ -0,0 +1,8 @@
+import heroQuickEats from "../assets/heroQuickEats.png";
+const Hero = () => {
+  return (
+    <img src={heroQuickEats} className="w-full max-h-[600px] object-cover" />
+  );
+};
+
+export default Hero;
---
+++ src/components/ui/MainNav.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./button";
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold text-xl hover:text-orange-500 hover:bg-white"
+    >
+      Log In
+    </Button>
+  );
+};
+
+export default MainNav;
